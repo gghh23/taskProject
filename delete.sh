700a2eb8acc89af9e042018972c7638eafe9c38a
gghh23 <lodin.dmitriy@gmail.com>
2024-01-03 15:04:05 +0300

    last version

--- delete.sh
+++ delete.sh
@@ -2,5 +2,5 @@ docker stop apache2host
 docker stop nginxt14
 docker rm apache2host
 docker rm nginxt14
-docker rmi -f t14apache:v1.0
-docker rmi -f t14nginx:v1.0
+docker rmi -f t14apache:latest
+docker rmi -f t14nginx:latest
