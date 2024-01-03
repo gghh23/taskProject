c630f99ecd884377387831c87593ab396f1ed5f8
gghh23 <lodin.dmitriy@gmail.com>
2024-01-03 14:29:29 +0300

    n

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ docker build -t t14apache:v1.0 .
 #docker run --network Net1 --name apache2host -p 8000:8000 -d t14apache:v1.0
 cd ../nginx
 docker build -t t14nginx:v1.0 .
-#docker run --network Net1 --name nginxt14 -d -p 80:80 -p 443:443 t14nginx:v1.0
+#docker run --network Net1 --name nginxt14 -d -p 80:80 -p 443:443 t14nginx:v1.0 f
 
